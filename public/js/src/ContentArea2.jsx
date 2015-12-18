ba4a8032e6e3983dee861d8dfb06f9277f93c341
Nathan Sass <nsass711@gmail.com>
2015-12-18 18:25:59 +1100

    building DOM for room options

--- public/js/src/ContentArea2.jsx
+++ public/js/src/ContentArea2.jsx
@@ -9,6 +9,32 @@
 					<div className="actionableQuestion">
 						What is the shape of your room?
 					</div>
+					<div className="roomCardContainer">
+						<div className="roomCard">
+							<div className="roomShapeContainer">
+								<img src="/public/img/rectangular.svg" alt="logo"/>
+								<div>Rectangular</div>
+							</div>
+						</div>
+						<div className="roomCard">
+							<div className="roomShapeContainer">
+								<img src="/public/img/rectangular.svg" alt="logo"/>
+								<div>Rectangular</div>
+							</div>
+						</div>
+						<div className="roomCard">
+							<div className="roomShapeContainer">
+								<img src="/public/img/rectangular.svg" alt="logo"/>
+								<div>Rectangular</div>
+							</div>
+						</div>
+						<div className="roomCard">
+							<div className="roomShapeContainer">
+								<img src="/public/img/rectangular.svg" alt="logo"/>
+								<div>Rectangular</div>
+							</div>
+						</div>
+					</div>
 				</section>
 			)
 		}
