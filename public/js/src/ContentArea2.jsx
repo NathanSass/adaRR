85cb4ec859f34de271c4deb1e4eb75098a921598
Nathan Sass <nsass711@gmail.com>
2015-12-18 22:53:50 +1100

    ready to break Dom elements out into components and then hook up react events and delegate events to network calls for backend.

--- public/js/src/ContentArea2.jsx
+++ public/js/src/ContentArea2.jsx
@@ -6,34 +6,37 @@
 
 			return (
 				<section className="contentArea2">
-					<div className="actionableQuestion">
-						What is the shape of your room?
-					</div>
-					<div className="roomCardContainer">
-						<div className="roomCard">
-							<div className="roomShapeContainer">
-								<img src="/public/img/rectangular.svg" alt="logo"/>
-								<div>Rectangular</div>
-							</div>
+					<div className="contentArea">
+						<div className="actionableQuestion">
+							What is the shape of your room?
 						</div>
-						<div className="roomCard">
-							<div className="roomShapeContainer">
-								<img src="/public/img/rectangular.svg" alt="logo"/>
-								<div>Rectangular</div>
-							</div>
+						<div className="roomCardContainer">
+							<a href="#" className="roomCard">
+								<div className="roomShapeContainer">
+									<img src="/public/img/rectangular.svg" alt="logo"/>
+									<div>Rectangular</div>
+								</div>
+							</a>
+							<a href="#" className="roomCard">
+								<div className="roomShapeContainer">
+									<img src="/public/img/rectangular.svg" alt="logo"/>
+									<div>Indent top right</div>
+								</div>
+							</a>
+							<a href="#" className="roomCard">
+								<div className="roomShapeContainer">
+									<img src="/public/img/rectangular.svg" alt="logo"/>
+									<div>Indent top left</div>
+								</div>
+							</a>
+							<a href="#" className="roomCard">
+								<div className="roomShapeContainer">
+									<img src="/public/img/rectangular.svg" alt="logo"/>
+									<div>Indent bottom right</div>
+								</div>
+							</a>
 						</div>
-						<div className="roomCard">
-							<div className="roomShapeContainer">
-								<img src="/public/img/rectangular.svg" alt="logo"/>
-								<div>Rectangular</div>
-							</div>
-						</div>
-						<div className="roomCard">
-							<div className="roomShapeContainer">
-								<img src="/public/img/rectangular.svg" alt="logo"/>
-								<div>Rectangular</div>
-							</div>
-						</div>
+						<button className="nextButton" type="button">Next</button>
 					</div>
 				</section>
 			)
