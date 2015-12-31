df866f5ee1856391e808cb87e8efcbdaff795897
Nathan Sass <nsass711@gmail.com>
2015-12-31 16:04:32 +1100

    making server in same file

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(favicon(__dirname + '/favicon.ico'));
 
-console.log("------  app file being read:");
+console.log("------  app1 file being read:");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -53,9 +51,111 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+
+///////////////////////////////
+
+
+
+/**
+ * Module dependencies.
+ */
+
+// var debug = require('debug')('swag:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '5000');
+// var port = normalizePort(process.env.PORT || '3000');
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+/////////
+// app.set('port', (process.env.PORT || 5000));
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port ', app.get('port'));
+// });
+//////////
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.listen(process.env.PORT || 5000);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  // debug('Listening on ' + bind);
+}
+
 
 
 
