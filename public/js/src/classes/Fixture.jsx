2ccc8a4e82ac920fb8b57764c4390bd62f999794
Nathan Sass <nsass711@gmail.com>
2016-01-18 21:49:10 +0700

    Moved the boundary constructor to a the toilet, TODO implement a basic boundary constructor which centers the fixture on the loc.

--- public/js/src/classes/Fixture.jsx
+++ public/js/src/classes/Fixture.jsx
@@ -24,102 +24,16 @@ class Fixture extends Shape {
 		this.fixtureObj   = params.room[this.name];
 		this.fixtureImg   = {};
 		this.canvasOffset = params.canvasOffset;
-		this.orientation  = null;
-		
+
 		this.findImageAndBoundary( params );
 	}
+	
 	/*
 		Calculates the fixture boundary and the location of the fixture image
+		TODO Implement: Calculates boundary centered on the Fixture
 	*/
-
 	findImageAndBoundary (params) {
-		var name         = this.name;
-		var note         = this.note;
-		var fixtureObj   = this.fixtureObj;
-		var fixtureImg   = this.fixtureImg;
-		var orientation  = this.orientation;
-		var canvasOffset = this.canvasOffset;
-		
-		var rotation     = params.room.rotation;
-		var distFromWall = params.room[name].loc.distFromWall;
-		
-		this.x = fixtureObj.loc.x + canvasOffset;
-		this.y = fixtureObj.loc.y + canvasOffset;
-		
-		/*
-			Takes the toilet width & height.
-			Translates the orientation to match the rotation.
-			Selects the proper image and builds its proper location
-		*/
-		switch ( rotation ) {
-			case 0:
-				/*
-					Places the bounding rectangle in the proper spot depending if the fixture is left or right justified
-				*/
-				if (note.indexOf('1') >= 0) {
-					this.x += - distFromWall;
-				} else {
-					this.x += - fixtureObj.bound.w + distFromWall;
-				}
-				/*
-					Places the image so it is visually centered with the loc coordinates
-				*/
-				orientation    = 1;
-				fixtureImg.img = document.getElementById( name + orientation);
-				fixtureImg.x   = fixtureObj.loc.x +  fixtureImg.img.width / 2;
-				fixtureImg.y   = fixtureObj.loc.y +  fixtureImg.img.height;
-				break;
-			case 90:
-				if (note.indexOf('1') >= 0) {
-					this.x += - fixtureObj.bound.h;
-					this.y += - distFromWall;																																				
-				} else {
-					this.x += - fixtureObj.bound.h;
-					this.y += - fixtureObj.bound.w + distFromWall;																																				
-				}
-				
-				orientation   = 2;
-				fixtureImg.img = document.getElementById( name + orientation);
-				fixtureImg.x   = fixtureObj.loc.x;
-				fixtureImg.y   = fixtureObj.loc.y + fixtureImg.img.height / 2;
-				break;
-			case 180:
-				if (note.indexOf('1') >= 0) {
-					this.x += - fixtureObj.bound.w + distFromWall;
-					this.y += - fixtureObj.bound.h;
-				} else {
-					this.x += - distFromWall;
-					this.y += - fixtureObj.bound.h;
-				}
-				
-				orientation = 3;
-				fixtureImg.img = document.getElementById( name + orientation);
-				fixtureImg.x   = fixtureObj.loc.x + fixtureImg.img.width / 2;
-				fixtureImg.y   = fixtureObj.loc.y;
-				break;
-			case 270:
-				if (note.indexOf('1') >= 0) {
-					this.y += -fixtureObj.bound.w + distFromWall;																																				
-				} else {
-					this.y -= distFromWall;																																				
-				}
-				
-				orientation = 4;
-				fixtureImg.img = document.getElementById( name + orientation);
-				fixtureImg.x   = fixtureObj.loc.x + fixtureImg.img.height;
-				fixtureImg.y   = fixtureObj.loc.y + fixtureImg.img.width / 2;
-				break;
-		}
-		
-
-		if ( rotation === 0 || rotation === 180 ) {
-			this.w = fixtureObj.bound.w;
-			this.h = fixtureObj.bound.h;
-		} else {
-			this.w = fixtureObj.bound.h;
-			this.h = fixtureObj.bound.w;
-		}
-
+		alert('Need to implement');
 	}
 	
 	/*
@@ -129,8 +43,10 @@ class Fixture extends Shape {
 	draw(ctx) {
 		
 		ctx.drawImage( this.fixtureImg.img, // draws image
+			
 			this.fixtureImg.x,
 			this.fixtureImg.y
+		
 		);
 		
 		ctx.beginPath(); // Draws bounding rectangle
--- public/js/src/classes/Toilet.jsx
+++ public/js/src/classes/Toilet.jsx
@@ -3,9 +3,91 @@ import { Fixture } from "./Fixture.jsx";
 
 class Toilet extends Fixture {
 	constructor(params) {
-		var superParams = params;
-		superParams.name = 'toilet';
-		super(superParams);
+		super(params);
+	}
+
+	/* Overrides default */
+	findImageAndBoundary(params) {
+		
+		var fixtureObj   = this.fixtureObj;
+		var fixtureImg   = this.fixtureImg;
+		var canvasOffset = this.canvasOffset;
+		
+		var rotation     = params.room.rotation;
+		var distFromWall = params.room[this.name].loc.distFromWall;
+		
+		this.x = fixtureObj.loc.x + canvasOffset;
+		this.y = fixtureObj.loc.y + canvasOffset;
+		
+		/*
+			Takes the fixture width & height.
+			Translates the orientation to match the rotation.
+			Selects the proper image and builds its proper location
+		*/
+		switch ( rotation ) {
+			case 0:
+				/*
+					Places the bounding rectangle in the proper spot depending if the fixture is left or right justified
+				*/
+				if (this.note.indexOf('1') >= 0) {
+					this.x += - distFromWall;
+				} else {
+					this.x += - fixtureObj.bound.w + distFromWall;
+				}
+				/*
+					Places the image so it is visually centered with the loc coordinates
+					The number coordinates to image naming standards
+				*/
+				fixtureImg.img = document.getElementById( this.name + 1);
+				fixtureImg.x   = fixtureObj.loc.x +  fixtureImg.img.width / 2;
+				fixtureImg.y   = fixtureObj.loc.y +  fixtureImg.img.height;
+				break;
+			case 90:
+				if (this.note.indexOf('1') >= 0) {
+					this.x += - fixtureObj.bound.h;
+					this.y += - distFromWall;																																				
+				} else {
+					this.x += - fixtureObj.bound.h;
+					this.y += - fixtureObj.bound.w + distFromWall;																																				
+				}
+				
+				fixtureImg.img = document.getElementById( this.name + 2);
+				fixtureImg.x   = fixtureObj.loc.x;
+				fixtureImg.y   = fixtureObj.loc.y + fixtureImg.img.height / 2;
+				break;
+			case 180:
+				if (this.note.indexOf('1') >= 0) {
+					this.x += - fixtureObj.bound.w + distFromWall;
+					this.y += - fixtureObj.bound.h;
+				} else {
+					this.x += - distFromWall;
+					this.y += - fixtureObj.bound.h;
+				}
+				
+				fixtureImg.img = document.getElementById( this.name + 3);
+				fixtureImg.x   = fixtureObj.loc.x + fixtureImg.img.width / 2;
+				fixtureImg.y   = fixtureObj.loc.y;
+				break;
+			case 270:
+				if (this.note.indexOf('1') >= 0) {
+					this.y += -fixtureObj.bound.w + distFromWall;																																				
+				} else {
+					this.y -= distFromWall;																																				
+				}
+				
+				fixtureImg.img = document.getElementById( this.name + 4);
+				fixtureImg.x   = fixtureObj.loc.x + fixtureImg.img.height;
+				fixtureImg.y   = fixtureObj.loc.y + fixtureImg.img.width / 2;
+				break;
+		}
+		
+		if ( rotation === 0 || rotation === 180 ) {
+			this.w = fixtureObj.bound.w;
+			this.h = fixtureObj.bound.h;
+		} else {
+			this.w = fixtureObj.bound.h;
+			this.h = fixtureObj.bound.w;
+		}
 	}
 }
 
